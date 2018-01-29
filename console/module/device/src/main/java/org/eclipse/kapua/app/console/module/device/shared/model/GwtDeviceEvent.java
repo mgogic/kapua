9e087c3539d033de5b54cd64178d5945fac5fc65
Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>
2018-01-29 15:21:02 +0100

    Assets tab renamed
    Signed-off-by: Claudio Mezzasalma <claudio.mezzasalma@eurotech.com>

--- console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/GwtDeviceEvent.java
+++ console/module/device/src/main/java/org/eclipse/kapua/app/console/module/device/shared/model/GwtDeviceEvent.java
@@ -15,7 +15,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 import com.google.gwt.user.client.rpc.IsSerializable;
-import org.eclipse.kapua.app.console.module.api.client.util.DateUtils;
 import org.eclipse.kapua.app.console.module.api.shared.model.KapuaBaseModel;
 
 public class GwtDeviceEvent extends KapuaBaseModel implements Serializable {
